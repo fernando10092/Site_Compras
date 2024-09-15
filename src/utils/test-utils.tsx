28c59414fadf17b91559cd2f7410327e2af0db36
Fernando Lima <fernando100@windowslive.com>
2024-09-15 18:40:56 -0300

    teste

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -3,11 +3,18 @@ import React from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../store'; // ajuste o caminho do seu store
+import userEvent from '@testing-library/user-event';
 
 const renderWithProviders = (
   ui: React.ReactElement,
   options?: RenderOptions
-) => render(<Provider store={store}>{ui}</Provider>, options);
+) => {
+  const utils = render(<Provider store={store}>{ui}</Provider>, options);
+  return {
+    ...utils,
+    user: userEvent.setup() // Adiciona `user` ao objeto retornado
+  };
+};
 
 export * from '@testing-library/react';
 export { renderWithProviders };
